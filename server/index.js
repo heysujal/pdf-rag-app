0a1b1c233e7ab57df7a5f2fc680cfc4b00925fba
Sujal Gupta <sujalgupta6100@gmail.com>
2026-01-12 01:13:03 +0530

    remove hardcoded PORT
    Signed-off-by: Sujal Gupta <sujalgupta6100@gmail.com>

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ if (!fs.existsSync("uploads")) {
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cors({
